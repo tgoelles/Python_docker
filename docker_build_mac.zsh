fd1209e3912ade842a581939083cabae79c7b600
thomasgoelles <thomas.goelles@gmail.com>
2021-11-29 13:44:00 +0100

    docker build script

--- docker_build_mac.zsh
+++ docker_build_mac.zsh
@@ -3,4 +3,5 @@
 tag="v0.0.1"
 
 docker build --platform linux/amd64,linux/arm64 --rm -f "Dockerfile" -t tgoelles/python_base:$tag "."
+#docker build --rm -f "Dockerfile" -t tgoelles/python_base:$tag "."
 #docker push tgoelles/python_base:$tag
