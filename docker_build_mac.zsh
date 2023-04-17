d6bb40fecc905b282083ef32a4e915b0fa411902
tgoelles <thomas.goelles@gmail.com>
2023-04-17 13:34:28 +0200

    test build

--- docker_build_mac.zsh
+++ docker_build_mac.zsh
@@ -2,7 +2,7 @@
 
 # just for local testing. Everthing is build wiht github actions
 
-tag="v0.5.0"
+tag="v0.5.2"
 
 docker build --rm -f "Dockerfile" --build-arg PYTHON_VERSION=3.10 -t tgoelles/python_base:$tag "."
 docker run -it tgoelles/python_base:$tag
